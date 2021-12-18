41599ded6865355426bcafb16b69b0d9cb6381a7
krishna9366 <71908847+krishna9366@users.noreply.github.com>
2021-12-18 15:18:01 -0500

    commit changes

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/* PERSONAL HOMEPAGE */
+
+console.log('Welcome to my personal homepage!')
+
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
